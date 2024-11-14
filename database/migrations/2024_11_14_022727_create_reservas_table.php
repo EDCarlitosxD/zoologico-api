425314ba61d6c36eab281b8c48135ead2cd593ba
EDCarlitosxD <juanuchdzib@gmail.com>
2024-11-13 21:28:55 -0500

    Feat(Reservas) Reservas Migration

---
+++ database/migrations/2024_11_14_022727_create_reservas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_usuario');
+            $table->integer('cantidad_personas')->nullable();
+            $table->unsignedBigInteger('id_horario_recorrido');
+
+            // Índices y claves foráneas
+            $table->foreign('id_usuario')
+                  ->references('id')
+                  ->on('usuarios')
+                  ->onDelete('no action')
+                  ->onUpdate('cascade');
+            $table->foreign('id_horario_recorrido')
+                  ->references('id')
+                  ->on('horario_recorridos')
+                  ->onDelete('no action')
+                  ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
