3ef4de1d88d96f29702728fdfdd7886165667d99
EDCarlitosxD <juanuchdzib@gmail.com>
2024-11-27 20:46:30 -0500

    Feat(HorarioRecorridoController) Add route for get All Horarios where Id recorrido

--- app/Http/Controllers/HorarioRecorridoController.php
+++ app/Http/Controllers/HorarioRecorridoController.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Models\HorarioRecorrido;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
 
 class HorarioRecorridoController
 {
     //
 
-    public function addNuevoHorario(Request $request){
+    public function addNuevoHorario(Request $request)
+    {
         $request->validate([
             'horario_inicio' => 'time|required',
             'horario_fin' => 'time|required',
@@ -25,15 +27,26 @@ class HorarioRecorridoController
         $datos = $request->all();
         $horarioRecorrido = new HorarioRecorrido($datos);
         $horarioRecorrido->save();
-        return response(['data' => $horarioRecorrido],Response::HTTP_CREATED);
+        return response(['data' => $horarioRecorrido], Response::HTTP_CREATED);
     }
 
 
-    public function getByIdRecorrido($id){
-        return HorarioRecorrido::where('id_recorrido',$id)->get();
+    public function getHorariosGroupByRecorridos($id)
+    {
+        $recorridos = HorarioRecorrido::where('id_recorrido',$id)
+            ->get();
+
+        $agrupadosPorDia = $recorridos->groupBy('fecha')->map(function ($horarios, $fecha) {
+            return $horarios->map(function ($horario) {
+                return [
+                    'id' => $horario->id,
+                    'horario_inicio' => $horario->horario_inicio,
+                    'horario_fin' => $horario->horario_fin,
+                    'disponible' => $horario->disponible,
+                ];
+            });
+        });
+
+        return response()->json($agrupadosPorDia);
     }
-
-
-
-
 }
